f98a205edcee26e40d4aa574ca3d9fe73b55e5b4
Carlos Serrano <secarl77@gmail.com>
2025-06-24 13:57:35 -0400

    first commit to Jenkins

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     # SQLAlchemy Database URI
     SQLALCHEMY_DATABASE_URI = (
         
-"postgresql://postgres:DevOps2024*@localhost/webappdb"
+"postgresql://postgres:DevOps2024*@database-cdb-3375-final-project.cl2gqc6scvwd.ca-central-1.rds.amazonaws.com/webappdb"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
---
+++ terraform/outputs.tf
@@ -0,0 +1,5 @@
+output "vpc_id" {
+  description = "ID VPC created"
+  value       = aws_vpc.CDB_3375_vpc.id
+}
+

77667613eb3f4015f25e8b1184e6342bbfc5648d
Carlos Serrano <secarl77@gmail.com>
2025-07-14 22:18:18 -0400

    configuring sublime 11

--- tests/test_ui_app.py
+++ tests/test_ui_app.py
@@ -2,6 +2,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import time
 
 class LoginUITest(unittest.TestCase):
